caf3fb2f14dce9331a80940bcee85840279b3834
A00894243 <sjjoel25@gmail.com>
2016-11-13 02:10:40 -0800

    Update updated-data.sql

--- data/updated-data.sql
+++ data/updated-data.sql
@@ -1,154 +1,160 @@
--- phpMyAdmin SQL Dump
--- version 4.6.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Oct 19, 2016 at 12:10 AM
--- Server version: 5.7.13
--- PHP Version: 7.0.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `jimsjoint`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `Categories`;
-CREATE TABLE `Categories` (
-  `id` varchar(1) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `description` text NOT NULL,
-  `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `Categories` (`id`, `name`, `description`, `image`) VALUES
-('d', 'Drinks', 'Purees made from the finest of Venusian insects, government-inspected.', 'catd.png'),
-('m', 'Mains', 'Made from only the finest ingredients found deep in the Martian jungle, and harvested or slaughtered by academy-trained druids, we bring you 45 day aged premium "meat".', 'catm.png'),
-('s', 'Sides', 'Perfect accompaniments to our mains, these side dish pairings have been exclsisvely formulated by Ben & Jerry.', 'cats.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE `ci_sessions` (
-  `id` varchar(128) NOT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `data` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `menu`
---
-
-DROP TABLE IF EXISTS `Menu`;
-CREATE TABLE `Menu` (
-  `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `description` varchar(256) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `picture` varchar(100) NOT NULL,
-  `category` varchar(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `menu`
---
-
-INSERT INTO `Menu` (`id`, `name`, `description`, `price`, `picture`, `category`) VALUES
-(1, 'Cheese', 'Leave this raw milk, beefy and sweet cheese out for an hour before serving and pair with pear jam.', '2.95', '1.png', 's'),
-(2, 'Turkey', 'Roasted, succulent, stuffed, lovingly sliced turkey breast', '5.95', '2.png', 'm'),
-(6, 'Donut', 'Disgustingly sweet, topped with artery clogging chocolate and then sprinkled with Pixie dust', '1.25', '6.png', 's'),
-(10, 'Bubbly', '1964 Moet Charmon, made from grapes crushed by elves with clean feet, perfectly chilled.', '14.50', '10.png', 'd'),
-(11, 'Ice Cream', 'Combination of decadent chocolate topped with luscious strawberry, churned by gifted virgins using only cream from the Tajima strain of wagyu cattle', '3.75', '11.png', 's'),
-(8, 'Hot Dog', 'Pork trimmings mixed with powdered preservatives, flavourings, red colouring and drenched in water before being squeezed into plastic tubes. Topped with onions, bacon, chili or cheese - no extra charge.', '6.90', '8.png', 'm'),
-(25, 'Burger', 'Half-pound of beef, topped with bacon and served with your choice of a slice of American cheese, red onion, sliced tomato, and Heart Attack Grill\'s own unique special sauce.', '9.99', 'burger.png', 'm'),
-(21, 'Coffee', 'A delicious cup of the nectar of life, saviour of students, morning kick-starter; made with freshly grounds that you don\'t want to know where they came from!', '2.95', 'coffee.png', 'd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderitems`
---
-
-DROP TABLE IF EXISTS `Orderitems`;
-CREATE TABLE `Orderitems` (
-  `order` int(11) NOT NULL,
-  `item` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `special` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `Orders`;
-CREATE TABLE `Orders` (
-  `num` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `status` varchar(1) NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `customer` varchar(64) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `Categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD KEY `ci_sessions_timestamp` (`timestamp`);
-
---
--- Indexes for table `menu`
---
-ALTER TABLE `Menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orderitems`
---
-ALTER TABLE `Orderitems`
-  ADD PRIMARY KEY (`order`,`item`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `Orders`
-  ADD PRIMARY KEY (`num`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+@@ -1,159 +0,0 @@
+--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+---
+--- Host: localhost    Database: view_lab
+--- ------------------------------------------------------
+--- Server version	5.7.10-log
+-
+-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-/*!40101 SET NAMES utf8 */;
+-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+-/*!40103 SET TIME_ZONE='+00:00' */;
+-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-
+---
+--- Table structure for table `categories`
+---
+-use views;
+-
+-DROP TABLE IF EXISTS `categories`;
+-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-/*!40101 SET character_set_client = utf8 */;
+-CREATE TABLE `categories` (
+-  `id` varchar(1) NOT NULL,
+-  `name` varchar(64) NOT NULL,
+-  `description` text NOT NULL,
+-  `image` varchar(100) NOT NULL,
+-  PRIMARY KEY (`id`)
+-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-/*!40101 SET character_set_client = @saved_cs_client */;
+-
+---
+--- Dumping data for table `categories`
+---
+-
+-LOCK TABLES `categories` WRITE;
+-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+-INSERT INTO `categories` VALUES ('d','Drinks','Purees made from the finest of Venusian insects, government-inspected.','catd.png'),('m','Mains','Made from only the finest ingredients found deep in the Martian jungle, and harvested or slaughtered by academy-trained druids, we bring you 45 day aged premium \"meat\".','catm.png'),('s','Sides','Perfect accompaniments to our mains, these side dish pairings have been exclsisvely formulated by Ben & Jerry.','cats.png');
+-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+-UNLOCK TABLES;
+-
+---
+--- Table structure for table `ci_sessions`
+---
+-
+-DROP TABLE IF EXISTS `ci_sessions`;
+-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-/*!40101 SET character_set_client = utf8 */;
+-CREATE TABLE `ci_sessions` (
+-  `id` varchar(128) NOT NULL,
+-  `ip_address` varchar(45) NOT NULL,
+-  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+-  `data` blob NOT NULL,
+-  KEY `ci_sessions_timestamp` (`timestamp`)
+-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-/*!40101 SET character_set_client = @saved_cs_client */;
+-
+---
+--- Dumping data for table `ci_sessions`
+---
+-
+-LOCK TABLES `ci_sessions` WRITE;
+-/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+-INSERT INTO `ci_sessions` VALUES ('63ae4a2f13faccff25cc064909fd3dbcbe45b41d','127.0.0.1',1478216662,'__ci_last_regenerate|i:1478216662;'),('87cbe260bdd0cac11882bfeeb25ed542a2a6f1aa','127.0.0.1',1478216988,'__ci_last_regenerate|i:1478216988;'),('281b60214085a7e29c264da3f934607b643af0c7','127.0.0.1',1478217299,'__ci_last_regenerate|i:1478217299;'),('3dde8364a2349f6f7a73258ec0073bb464369d5b','127.0.0.1',1478218213,'__ci_last_regenerate|i:1478218213;userrole|s:5:\"admin\";'),('888789e6787bc43aa88de9b3cbf3ac451e522b2b','127.0.0.1',1478218321,'__ci_last_regenerate|i:1478218213;userrole|s:5:\"admin\";');
+-/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+-UNLOCK TABLES;
+-
+---
+--- Table structure for table `menu`
+---
+-
+-DROP TABLE IF EXISTS `menu`;
+-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-/*!40101 SET character_set_client = utf8 */;
+-CREATE TABLE `menu` (
+-  `id` int(11) NOT NULL,
+-  `name` varchar(32) NOT NULL,
+-  `description` varchar(256) NOT NULL,
+-  `price` decimal(10,2) NOT NULL,
+-  `picture` varchar(100) NOT NULL,
+-  `category` varchar(1) NOT NULL,
+-  PRIMARY KEY (`id`)
+-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-/*!40101 SET character_set_client = @saved_cs_client */;
+-
+---
+--- Dumping data for table `menu`
+---
+-
+-LOCK TABLES `menu` WRITE;
+-/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+-INSERT INTO `menu` VALUES (1,'Cheese','Leave this raw milk, beefy and sweet cheese out for an hour before serving and pair with pear jam.',2.95,'1.png','s'),(2,'Turkey','Roasted, succulent, stuffed, lovingly sliced turkey breast',5.95,'2.png','m'),(6,'Donut','Disgustingly sweet, topped with artery clogging chocolate and then sprinkled with Pixie dust',1.25,'6.png','s'),(10,'Bubbly','1964 Moet Charmon, made from grapes crushed by elves with clean feet, perfectly chilled.',14.50,'10.png','d'),(11,'Ice Cream','Combination of decadent chocolate topped with luscious strawberry, churned by gifted virgins using only cream from the Tajima strain of wagyu cattle',3.75,'11.png','s'),(8,'Hot Dog','Pork trimmings mixed with powdered preservatives, flavourings, red colouring and drenched in water before being squeezed into plastic tubes. Topped with onions, bacon, chili or cheese - no extra charge.',6.90,'8.png','m'),(25,'Burger','Half-pound of beef, topped with bacon and served with your choice of a slice of American cheese, red onion, sliced tomato, and Heart Attack Grill\'s own unique special sauce.',9.99,'burger.png','m'),(21,'Coffee','A delicious cup of the nectar of life, saviour of students, morning kick-starter; made with freshly grounds that you don\'t want to know where they came from!',2.95,'coffee.png','d');
+-/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+-UNLOCK TABLES;
+-
+---
+--- Table structure for table `orderitems`
+---
+-
+-DROP TABLE IF EXISTS `orderitems`;
+-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-/*!40101 SET character_set_client = utf8 */;
+-CREATE TABLE `orderitems` (
+-  `order` int(11) NOT NULL,
+-  `item` int(11) NOT NULL,
+-  `quantity` int(11) NOT NULL,
+-  `special` text NOT NULL,
+-  PRIMARY KEY (`order`,`item`)
+-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-/*!40101 SET character_set_client = @saved_cs_client */;
+-
+---
+--- Dumping data for table `orderitems`
+---
+-
+-LOCK TABLES `orderitems` WRITE;
+-/*!40000 ALTER TABLE `orderitems` DISABLE KEYS */;
+-/*!40000 ALTER TABLE `orderitems` ENABLE KEYS */;
+-UNLOCK TABLES;
+-
+---
+--- Table structure for table `orders`
+---
+-
+-DROP TABLE IF EXISTS `orders`;
+-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-/*!40101 SET character_set_client = utf8 */;
+-CREATE TABLE `orders` (
+-  `num` int(11) NOT NULL,
+-  `date` datetime NOT NULL,
+-  `status` varchar(1) NOT NULL,
+-  `total` decimal(10,2) NOT NULL,
+-  `customer` varchar(64) NOT NULL,
+-  PRIMARY KEY (`num`)
+-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-/*!40101 SET character_set_client = @saved_cs_client */;
+-
+---
+--- Dumping data for table `orders`
+---
+-
+-LOCK TABLES `orders` WRITE;
+-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+-UNLOCK TABLES;
+-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-
+-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-
+--- Dump completed on 2016-11-03 17:15:01
